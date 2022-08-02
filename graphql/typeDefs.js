55a1ff545af513dd7832877a60bf20a666f4bad9
Cyber-Kira <kirillkoloskov03@gmail.com>
2022-08-02 18:04:46 +0300

    feat: add likes and comments types

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -6,6 +6,23 @@ module.exports = gql`
 		body: String!
 		username: String!
 		createdAt: String!
+		comments: [Comment]!
+		likes: [Like]!
+		likeCount: Int!
+		commentCount: Int!
+	}
+
+	type Comment {
+		id: ID!
+		body: String!
+		username: String!
+		createdAt: String!
+	}
+
+	type Like {
+		id: ID!
+		username: String!
+		createdAt: String!
 	}
 
 	type Query {
@@ -33,5 +50,8 @@ module.exports = gql`
 		login(username: String!, password: String!): User!
 		createPost(body: String!): Post!
 		deletePost(postId: ID!): String!
+		createComment(postId: String!, body: String!): Post!
+		deleteComment(postId: ID!, commentId: ID!): Post!
+		likePost(postId: ID!): Post!
 	}
 `
