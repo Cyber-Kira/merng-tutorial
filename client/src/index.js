acbb3a30c15959e33e931632afaa34e6bcab9cf8
Cyber-Kira <kirillkoloskov03@gmail.com>
2022-08-03 22:41:55 +0300

    feat: add custom css

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import WithApolloProvider from './ApolloProvider'
 import App from './App'
 
 import 'semantic-ui-css/semantic.min.css'
+import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
