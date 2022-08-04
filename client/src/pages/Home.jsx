bd5b059dc199b90218798343080167b73d54c7c9
Cyber-Kira <kirillkoloskov03@gmail.com>
2022-08-04 16:15:46 +0300

    feat: add postForm if user is logged in and add transition to the posts

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,32 +1,13 @@
 import { useQuery } from '@apollo/client'
-import gql from 'graphql-tag'
-import React from 'react'
-import { Grid } from 'semantic-ui-react'
+import React, { useContext } from 'react'
+import { Grid, Transition } from 'semantic-ui-react'
 import PostCard from '../components/PostCard'
-
-const FETCH_POSTS_QUERY = gql`
-	{
-		getPosts {
-			id
-			body
-			createdAt
-			username
-			likeCount
-			likes {
-				username
-			}
-			commentCount
-			comments {
-				id
-				username
-				createdAt
-				body
-			}
-		}
-	}
-`
+import PostForm from '../components/PostForm'
+import { AuthContext } from '../context/Auth'
+import { FETCH_POSTS_QUERY } from '../utils/graphql'
 
 const Home = () => {
+	const { user } = useContext(AuthContext)
 	const { loading, data } = useQuery(FETCH_POSTS_QUERY)
 
 	return (
@@ -35,18 +16,25 @@ const Home = () => {
 				<h1>Recent Posts</h1>
 			</Grid.Row>
 			<Grid.Row>
+				{user && (
+					<Grid.Column>
+						<PostForm />
+					</Grid.Column>
+				)}
 				{loading ? (
 					<h1>Loading posts...</h1>
 				) : (
-					data.getPosts &&
-					data.getPosts.map(post => (
-						<Grid.Column
-							key={post.id}
-							style={{ marginBottom: '2rem', boxShadow: 'none' }}
-						>
-							<PostCard post={post} />
-						</Grid.Column>
-					))
+					<Transition.Group>
+						{data.getPosts &&
+							data.getPosts.map(post => (
+								<Grid.Column
+									key={post.id}
+									style={{ marginBottom: '2rem', boxShadow: 'none' }}
+								>
+									<PostCard post={post} />
+								</Grid.Column>
+							))}
+					</Transition.Group>
 				)}
 			</Grid.Row>
 		</Grid>
