8c5d962c2dd8f7c102282fca38a0dd01ae574ac9
Cyber-Kira <kirillkoloskov03@gmail.com>
2022-08-04 16:16:05 +0300

    refactoring: remove query to the separate file

---
+++ client/src/utils/graphql.js
@@ -0,0 +1,24 @@
+/* eslint-disable import/prefer-default-export */
+import gql from 'graphql-tag'
+
+export const FETCH_POSTS_QUERY = gql`
+	{
+		getPosts {
+			id
+			body
+			createdAt
+			username
+			likeCount
+			likes {
+				username
+			}
+			commentCount
+			comments {
+				id
+				username
+				createdAt
+				body
+			}
+		}
+	}
+`
