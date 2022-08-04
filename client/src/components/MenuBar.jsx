8259942c444c837c67e0a88cb366d246f63a9b15
Cyber-Kira <kirillkoloskov03@gmail.com>
2022-08-04 15:27:14 +0300

    feat: change menuBar based on authContext

--- client/src/components/MenuBar.jsx
+++ client/src/components/MenuBar.jsx
@@ -1,8 +1,11 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { Link, useLocation } from 'react-router-dom'
 import { Menu } from 'semantic-ui-react'
+import { AuthContext } from '../context/Auth'
 
 const MenuBar = () => {
+	const { user, logout } = useContext(AuthContext)
+
 	const location = useLocation()
 	const [activeItem, setActiveItem] = useState('')
 
@@ -13,35 +16,42 @@ const MenuBar = () => {
 
 	const handleItemClick = (_, { name }) => setActiveItem(name)
 
-	return (
-		<div>
-			<Menu pointing secondary size='massive' color='teal'>
+	const menuBarElement = user ? (
+		<Menu pointing secondary size='massive' color='teal'>
+			<Menu.Item name={user.username} active as={Link} to='/' />
+			<Menu.Menu position='right'>
+				<Menu.Item name='logout' onClick={logout} />
+			</Menu.Menu>
+		</Menu>
+	) : (
+		<Menu pointing secondary size='massive' color='teal'>
+			<Menu.Item
+				name='home'
+				active={activeItem === 'home'}
+				onClick={handleItemClick}
+				as={Link}
+				to='/'
+			/>
+			<Menu.Menu position='right'>
 				<Menu.Item
-					name='home'
-					active={activeItem === 'home'}
+					name='login'
+					active={activeItem === 'login'}
 					onClick={handleItemClick}
 					as={Link}
-					to='/'
+					to='/login'
 				/>
-				<Menu.Menu position='right'>
-					<Menu.Item
-						name='login'
-						active={activeItem === 'login'}
-						onClick={handleItemClick}
-						as={Link}
-						to='/login'
-					/>
-					<Menu.Item
-						name='register'
-						active={activeItem === 'register'}
-						onClick={handleItemClick}
-						as={Link}
-						to='/register'
-					/>
-				</Menu.Menu>
-			</Menu>
-		</div>
+				<Menu.Item
+					name='register'
+					active={activeItem === 'register'}
+					onClick={handleItemClick}
+					as={Link}
+					to='/register'
+				/>
+			</Menu.Menu>
+		</Menu>
 	)
+
+	return <div>{menuBarElement}</div>
 }
 
 export default MenuBar
