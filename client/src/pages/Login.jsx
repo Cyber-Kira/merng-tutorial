4e68a20a8e64dcdc0a10dd6a52f48164b7538366
Cyber-Kira <kirillkoloskov03@gmail.com>
2022-08-04 14:20:07 +0300

    feat: implement login and registration functionality

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,100 @@
-import React from 'react'
+import { useMutation, gql } from '@apollo/client'
+
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { Button, Form } from 'semantic-ui-react'
+import { useForm } from '../utils/hooks'
+
+const LOGIN_USER = gql`
+	mutation login($username: String!, $password: String!) {
+		login(username: $username, password: $password) {
+			id
+			email
+			username
+			createdAt
+			token
+		}
+	}
+`
 
 const Login = () => {
-	return <div>Login</div>
+	const navigate = useNavigate()
+	const [errors, setErrors] = useState({})
+
+	const initialState = {
+		username: '',
+		password: '',
+	}
+
+	const { onChange, onSubmit, values } = useForm(
+		loginUserCallback,
+		initialState
+	)
+
+	const [loginUser, { loading }] = useMutation(LOGIN_USER, {
+		update(_, result) {
+			// eslint-disable-next-line no-console
+			console.log(result)
+			setErrors({})
+			navigate('/', { replace: true })
+		},
+		onError(err) {
+			console.log(err.graphQLErrors[0].extensions.errors)
+			setErrors(err.graphQLErrors[0].extensions.errors)
+		},
+		variables: values,
+	})
+
+	function loginUserCallback() {
+		loginUser()
+	}
+
+	return (
+		<div className='form-container'>
+			<Form
+				onSubmit={onSubmit}
+				noValidate
+				className={loading ? 'loading' : null}
+			>
+				<h1>Login</h1>
+				<Form.Input
+					label='Username'
+					placeholder='Username...'
+					name='username'
+					type='text'
+					value={values.username}
+					onChange={onChange}
+					error={
+						errors.username || (errors.general && errors.general.length > 0)
+					}
+				/>
+
+				<Form.Input
+					label='Password'
+					placeholder='Password...'
+					name='password'
+					type='password'
+					value={values.password}
+					onChange={onChange}
+					error={
+						errors.password || (errors.general && errors.general.length > 0)
+					}
+				/>
+				<Button type='submit' primary>
+					Login
+				</Button>
+			</Form>
+			{Object.keys(errors).length > 0 && errors.general ? (
+				<div className='ui error message'>
+					<u className='list'>
+						{Object.values(errors).map(errorMessage => (
+							<li key={errorMessage}>{errorMessage}</li>
+						))}
+					</u>
+				</div>
+			) : null}
+		</div>
+	)
 }
 
 export default Login
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,7 +1,124 @@
-import React from 'react'
+import { useMutation, gql } from '@apollo/client'
+
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { Button, Form } from 'semantic-ui-react'
+import { useForm } from '../utils/hooks'
+
+const REGISTER_USER = gql`
+	mutation register(
+		$username: String!
+		$password: String!
+		$confirmPassword: String!
+		$email: String!
+	) {
+		register(
+			registerInput: {
+				username: $username
+				password: $password
+				confirmPassword: $confirmPassword
+				email: $email
+			}
+		) {
+			id
+			email
+			username
+			createdAt
+			token
+		}
+	}
+`
 
 const Register = () => {
-	return <div>Register</div>
+	const navigate = useNavigate()
+	const [errors, setErrors] = useState({})
+
+	const initialState = {
+		username: '',
+		email: '',
+		password: '',
+		confirmPassword: '',
+	}
+
+	const { onChange, onSubmit, values } = useForm(registerUser, initialState)
+
+	const [addUser, { loading }] = useMutation(REGISTER_USER, {
+		update() {
+			setErrors({})
+			navigate('/', { replace: true })
+		},
+		onError(err) {
+			setErrors(err.graphQLErrors[0].extensions.errors)
+		},
+		variables: values,
+	})
+
+	function registerUser() {
+		addUser()
+	}
+
+	return (
+		<div className='form-container'>
+			<Form
+				onSubmit={onSubmit}
+				noValidate
+				className={loading ? 'loading' : null}
+			>
+				<h1>Register</h1>
+				<Form.Input
+					label='Username'
+					placeholder='Username...'
+					name='username'
+					type='text'
+					value={values.username}
+					onChange={onChange}
+					error={errors.username}
+				/>
+
+				<Form.Input
+					label='Email'
+					placeholder='Email...'
+					name='email'
+					type='email'
+					value={values.email}
+					onChange={onChange}
+					error={errors.email}
+				/>
+
+				<Form.Input
+					label='Password'
+					placeholder='Password...'
+					name='password'
+					type='password'
+					value={values.password}
+					onChange={onChange}
+					error={errors.password}
+				/>
+
+				<Form.Input
+					label='Confirm Password'
+					placeholder='Confirm Password...'
+					name='confirmPassword'
+					type='password'
+					value={values.confirmPassword}
+					onChange={onChange}
+					error={errors.confirmPassword}
+				/>
+				<Button type='submit' primary>
+					Register
+				</Button>
+			</Form>
+			{/* {Object.keys(errors).length > 0 ? (
+				<div className='ui error message'>
+					<u className='list'>
+						{Object.values(errors).map(errorMessage => (
+							<li key={errorMessage}>{errorMessage}</li>
+						))}
+					</u>
+				</div>
+			) : null} */}
+		</div>
+	)
 }
 
 export default Register
